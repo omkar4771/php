46f8d1c48f506ef282ca9f316efa43468aa27ab1
omkar4771 <omkardaswadkar123@gmail.com>
2024-09-03 22:38:32 +0530

    basics of php

---
+++ basics.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Omkar</h1>
+
+
+    <?php
+
+        define("PI", 3.14); // define a constant value --> we can define string constant  as well as number constant
+
+        $my_var = (true and false);
+        echo var_dump($my_var);
+        echo "<br>";
+
+        $var = "omkar";
+        echo var_dump($var);
+        echo"<br>";
+
+        $var = 25;
+        echo var_dump($var);
+        echo"<br>";
+
+        $var1 = 61.1;
+        echo var_dump($var1);
+        echo"<br>";
+
+        echo PI;
+
+    ?>
+</body>
+</html>
