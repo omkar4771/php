80311a38081f4e772c389146453fe9e102abdcc7
omkar4771 <omkardaswadkar123@gmail.com>
2024-09-03 22:39:49 +0530

    loops

---
+++ loop.php
@@ -0,0 +1,21 @@
+<?php
+
+$language = ["C","C++","Pythhon","Java","Node Js", "Express Js","Dart","SQL","MongoDB","AWS","Git","VsCode"];
+
+$a=0;
+while($a<=10){
+    echo "The value of language is :$a"."\n";
+    $a++;
+}
+
+$b=0;
+while($b <= 10){
+    echo "the value of language is : $language[$b]"."\n";
+    $b++;
+}
+for ($i=2; $i < 10; $i++) { 
+    echo $i;
+}
+
+
+?>
