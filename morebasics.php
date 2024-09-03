d79daf81c8b88830b158b7cd800aaf55ef2b59c0
omkar4771 <omkardaswadkar123@gmail.com>
2024-09-03 22:40:10 +0530

    basics

---
+++ morebasics.php
@@ -0,0 +1,17 @@
+<?php
+
+$age = 6;
+
+if($age >= 18){
+    echo "You can go to party";
+}
+elseif($age == 6 ){
+    echo "Your age is 6 you can not go to party";
+}
+else if($age <=18){
+    echo "You can not go to party";
+}
+else{
+    echo "Your age is less than 18 youn can not go to party";
+}
+?>
