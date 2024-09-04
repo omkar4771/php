c07fe533f3dd44580fa0d99faa2cdf8a1bdf7629
omkar4771 <omkardaswadkar123@gmail.com>
2024-09-04 22:56:57 +0530

    php

---
+++ include1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>main page</title>
+</head>
+<body>
+    <?php
+    include("menu.html");
+    ?>
+    <h1>This is menue page </h1> 
+    <?php
+    include("body.html");
+    ?>
+</body> 
+</html>
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = $_POST["name"];
+$password = $_POST["password"];
+
+echo "welcome $name your password is $password";
+
+?>
