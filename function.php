f5ead90e9ff808a90393e9a7089ae5a884f42191
omkar4771 <omkardaswadkar123@gmail.com>
2024-09-05 22:14:51 +0530

    function

--- function.php
+++ function.php
@@ -1,5 +1,4 @@
 <?php
-
 function sum ($marks){
     $sum = 0;
     foreach($marks as $mark){
@@ -7,10 +6,15 @@ function sum ($marks){
     }
     return $sum;
 }
-
 $marks = array(15,20,36,45,20,98);
 $total = sum($marks);
+echo "total marks of roan is $total"."\n";
 
-echo "total marks of roan is $total";
-
+function display($num){
+    if($num <=5){
+        echo $num."\n";
+    }
+    display($num+1);
+}
+display(1);
 ?>
